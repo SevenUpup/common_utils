a0de247272159378273ffbe3e8b4c19cd9ad5c58
FiDo <ilikesevenup@163.com>
2023-07-28 09:50:20 +0800

    common_base_util: logExt新增 logD打印长日志方法 common_base_ui  :

--- common_base_util/src/main/java/com/fido/common/common_base_util/ext/LogExt.kt
+++ common_base_util/src/main/java/com/fido/common/common_base_util/ext/LogExt.kt
@@ -1,5 +1,6 @@
 package com.fido.common.common_base_util.ext
 
+import android.util.Log
 import com.fido.common.common_base_util.log.LogUtils
 
 /**
@@ -26,4 +27,20 @@ fun logGlobalTag(global: String) {
 
 fun logEnabled(logEnabled: Boolean) {
     LogUtils.logEnabled = logEnabled
+}
+
+
+/**
+ * 支持超长日志输出的工具方法
+ * */
+private const val SEGMENT_SIZE = 3072
+fun logD(content: String) {
+    if(!LogUtils.logEnabled) return
+    if (content.length < SEGMENT_SIZE) {
+        Log.d(LogUtils.logGlobalTag, content)
+        return
+    } else {
+        Log.d(LogUtils.logGlobalTag, content.substring(0, SEGMENT_SIZE))
+        logD(content.substring(SEGMENT_SIZE))
+    }
 }
